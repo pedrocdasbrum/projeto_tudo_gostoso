52252608584f598ad6cc05524bb5720d7a6ead6d
Pedro Caetano <pedrocdasbrum@gmail.com>
2021-05-13 14:43:40 -0300

    Commit N2

---
+++ android/app/src/main/kotlin/br/com/pedrocaetano/tudo_gostoso/MainActivity.kt
@@ -0,0 +1,6 @@
+package br.com.pedrocaetano.tudo_gostoso
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
--- style.dart
+++
@@ -1,3 +0,0 @@
-import 'package:flutter/painting.dart';
-
-Color orangeTheme = Color(0xFFFF3D0A);
